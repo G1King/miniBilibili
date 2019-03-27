ab8d1d911345543b69269d844b806ddcd56ddd97
Leo <G1King@163.com>
2019-03-27 13:46:06 +0800

    开发详情

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,17 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+videoInfo:null,
+    othersList:[],
+commentData:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-console.log(options)
-
+    let videoId = options.id;
+    this.getVideoInfo(videoId);
+    this.getOthersList(videoId);
+    this.getCommentList(videoId);
+  },
+  /**
+   * 获取视频详情
+   */
+getVideoInfo(videoId){
+ wx.request({
+   url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/videoDetail?id="+videoId,
+   success:(v)=> {
+     
+     this.setData({
+       videoInfo:v.data.data.videoInfo
+     })
+   }
+ })
+},
+  /**
+     * 获取评论数据
+     */
+  getCommentList(videoId) {
+    let that = this;
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/commentsList?id=" + videoId,
+      success(res) {
+        console.log(res);
+        if (res.data.code === 0) {
+          that.setData({
+            commentData: res.data.data.commentData
+          })
+        }
+      }
+    })
   },
 
+  /**
+    * 获取推荐视频
+    */
+  getOthersList(videoId) {
+    let that = this;
+    wx.request({
+      url: "https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/othersList?id=" + videoId,
+      success(res) {
+        console.log(res);
+        if (res.data.code === 0) {
+          that.setData({
+            othersList: res.data.data.othersList
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
